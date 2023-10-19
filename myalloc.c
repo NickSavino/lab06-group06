446b4a06663c71abf652d492310237ec97b75d55
Nicola <nicksavino2@gmail.com>
2023-10-19 20:28:22 +0000

    Added Makefile and skeleton functions

--- myalloc.c
+++ myalloc.c
@@ -1,1 +1,18 @@
-Place code here.
+#include <stddef.h>
+#include "myalloc.h"
+
+extern int myinit(size_t size) {
+
+}
+
+extern int mydestroy() {
+
+}
+
+extern void* myalloc(size_t size) {
+
+}
+
+extern void myfree(void *ptr) {
+
+}
